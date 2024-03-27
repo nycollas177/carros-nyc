64e839fb8c4649b69b8e10882f02d9100ad49208
Nicollas177 <nycollascruz5@gmail.com>
2024-03-26 21:42:16 -0300

    quarto commit do App

--- src/App.jsx
+++ src/App.jsx
@@ -7,26 +7,25 @@ return(
   <section>
     <div className="bg-orange">
   <img src={iconSedans} alt="Icones do carros sedan" />
-  <h2>Sedans</h2>
-  <p>Escolha um sedan pelo seu preço acessível e excelente economia de combustível. Ideal para dirigir na cidade ou na sua próxima viagem.</p>
-  <a href="https://www.webmotors.com.br/sedans" target="_blank">Ver mais</a>
+  <h2 className="font-shoulders">Sedans</h2>
+  <p className="font-lexend">Escolha um sedan pelo seu preço acessível e excelente economia de combustível. Ideal para dirigir na cidade ou na sua próxima viagem.</p>
+  <a className="font-lexend" href="https://www.webmotors.com.br/sedans" target="_blank">Ver mais</a>
     </div>
 
     <div className="bg-green">
 <img src={iconSuvs} alt="Icones do carros SUVs" />
-<h2>SUVs</h2>
-<p>Escolha um SUV pelo seu interior espaçoso, poder e versatilidade. Perfeito para passar as férias em família ou para aventuras off-road</p>
-<a href="https://www.webmotors.com.br/suvs" target="_blank">Ver mais</a>
+<h2 className="font-shoulders">SUVs</h2>
+<p className="font-lexend">Escolha um SUV pelo seu interior espaçoso, poder e versatilidade. Perfeito para passar as férias em família ou para aventuras off-road</p>
+<a className="font-lexend" href="https://www.webmotors.com.br/suvs" target="_blank">Ver mais</a>
     </div>
 
     <div className="bg-dark-green">
       <img src={iconLuxury} alt="Icones do carros luxo" />
-      <h2>Luxuosos</h2>
-      <p>Passeie nos melhores carros sem os preços exorbitantes. Desfrute do conforto aprimorado de um aluguel de luxo e chegue com estilo.</p>
-      <a href="https://www.webmotors.com.br/carros-de-luxo" target="_blank">Ver mais</a>
+      <h2 className="font-shoulders">Luxuosos</h2>
+      <p className="font-lexend">Passeie nos melhores carros sem os preços exorbitantes. Desfrute do conforto aprimorado de um aluguel de luxo e chegue com estilo.</p>
+      <a className="font-lexend" href="https://www.webmotors.com.br/carros-de-luxo" target="_blank">Ver mais</a>
     </div>
   </section>
 </main>
 )
-
 }
