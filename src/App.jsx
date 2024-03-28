8bcbd00b2fff1c704e04cf108d58d7527f86a76f
Nicollas177 <nycollascruz5@gmail.com>
2024-03-27 22:05:28 -0300

    projeto finalizado

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,11 @@ import Luxuosos from "./Component/Luxuosos"
 export default function App() {
 return(
 <main className="min-h-shv bg-offwhitebg py-20 px-6 antialiased lg:flex justify-center items-center">
-  <section className="lg:flex max-w-[920px]">
-  
-  <Sedans />
-  <Suvs />
-  <Luxuosos />
-  </section>
+  <div className="lg:flex max-w-[920px]">
+    <Sedans />
+    <Suvs />
+    <Luxuosos />
+  </div>
 </main>
 )
 }
